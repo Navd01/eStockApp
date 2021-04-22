620fb4501893fd488b484547748df666dc27736e
MohammadAbdul Naveed <mohammednaveed01@gmail.com>
2021-04-22 11:13:52 +0530

    eStockApp

---
+++ src/main/java/com/Naveed/eStockApp/EStockAppApplication.java
@@ -0,0 +1,13 @@
+package com.Naveed.eStockApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EStockAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EStockAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Naveed/eStockApp/EStockAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Naveed.eStockApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EStockAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
